99e443b031fe11152e761824d18f7788acb6cd03
jonandermorenoo88 <jaincera97@gmail.com>
2025-10-28 21:53:47 +0100

    Inicial: estructura base del proyecto Maven

---
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/IsoAssetManagerApplication.java
@@ -0,0 +1,12 @@
+package es.ubu.gii.ISOAssetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoAssetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsoAssetManagerApplication.class, args);
+    }
+}
---
+++ ISOAssetManager/src/test/java/es/ubu/gii/ISOAssetManager/IsoAssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ubu.gii.ISOAssetManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoAssetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
