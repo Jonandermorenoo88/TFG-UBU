9e7d59f63a60d446f2de92d7a489f86b0501e17a
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-12 13:44:23 +0100

    creando el controlador del usuario

---
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/UsuarioController.java
@@ -0,0 +1,22 @@
+package es.ubu.gii.ISOAssetManager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+
+    @GetMapping("/inicio")
+    public String mostrarInicio(Model model) {
+        return "inicio"; 
+    }
+
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
+    }
+}
+
+
+
