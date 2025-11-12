ec0e68663c2e57a2522d3ee91d546ae12068ed7c
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-12 13:59:13 +0100

    arreglando el inicio

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/UsuarioController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/UsuarioController.java
@@ -11,11 +11,6 @@ public class UsuarioController {
     public String mostrarInicio(Model model) {
         return "inicio"; 
     }
-
-    @GetMapping("/registro")
-    public String registro() {
-        return "registro";
-    }
 }
 
 
