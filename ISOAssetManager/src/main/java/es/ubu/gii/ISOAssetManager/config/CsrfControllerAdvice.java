edfca956ef6d5c6645ffb8c9e172a786d48c2920
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-14 08:47:09 +0100

    ajuste final de la carpeta config

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/config/CsrfControllerAdvice.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/config/CsrfControllerAdvice.java
@@ -1,5 +1,6 @@
 package es.ubu.gii.ISOAssetManager.config;
 
 public class CsrfControllerAdvice {
+    // 
     
 }
---
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package es.ubu.gii.ISOAssetManager.config;
+
+public class SecurityConfig {
+    
+}
