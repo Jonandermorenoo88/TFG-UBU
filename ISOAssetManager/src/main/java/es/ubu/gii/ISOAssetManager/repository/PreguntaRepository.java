8d7a084e7b6b8fbb7a838bb96c3f072db62b203f
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-16 20:49:03 +0100

    realizando  la clase de preguntas en repositoio

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/PreguntaRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/PreguntaRepository.java
@@ -1,5 +1,12 @@
+// PreguntaRepository.java
 package es.ubu.gii.ISOAssetManager.repository;
 
-public class PreguntaRepository {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import es.ubu.gii.ISOAssetManager.model.Pregunta;
+
+public interface PreguntaRepository extends JpaRepository<Pregunta, Long> {
+    List<Pregunta> findByControl_Id(String controlId);
+    List<Pregunta> findByControl_Categoria_Id(String categoriaId);
+    List<Pregunta> findByControl_IdOrderByIdAsc(String controlId);
 }
