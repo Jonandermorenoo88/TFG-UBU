c5f2ae10f81184983e53bb82f5fa4b9247e7059f
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-16 20:04:44 +0100

    realizando el login controller

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/LoginController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/LoginController.java
@@ -1,5 +1,15 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
 public class LoginController {
-    
+
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login";
+    }
 }
