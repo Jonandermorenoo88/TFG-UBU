81d970af55926834f69755e09d216b2e071c1d38
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-16 19:12:20 +0100

    realizando la clase departamento en el repositorio

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/DepartamentoRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/DepartamentoRepository.java
@@ -1,5 +1,13 @@
+// DepartamentoRepository.java
 package es.ubu.gii.ISOAssetManager.repository;
 
-public class DepartamentoRepository {
-    
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import es.ubu.gii.ISOAssetManager.model.Departamento;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+    // Busca por descripción dentro de una empresa concreta
+    Optional<Departamento> findByDescripcionAndEmpresaId(String descripcion, Long empresaId);
 }
+
