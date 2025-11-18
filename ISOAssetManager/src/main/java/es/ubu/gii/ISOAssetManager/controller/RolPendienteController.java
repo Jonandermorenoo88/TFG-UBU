6a2e438a8cc7defc9e5c68f4131685e2d8361d3a
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-18 22:43:30 +0100

    realizando la clase RolPendienteController

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/RolPendienteController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/RolPendienteController.java
@@ -1,5 +1,15 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RolPendienteController {
+
+    @GetMapping("/esperarol")
+    public String esperarRol() {
+        return "esperarol";
+    }
+
     
 }
