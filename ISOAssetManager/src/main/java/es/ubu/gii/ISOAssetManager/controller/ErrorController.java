9ad1501cdf913d49b07bb159a5baa24a83c4ee5f
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-19 22:50:44 +0100

    creando método para manejar el error 403 en ErrorController

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/ErrorController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/ErrorController.java
@@ -1,5 +1,17 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
 public class ErrorController {
-    
+
+    // Error 403 - Acceso denegado
+    @GetMapping("/error/403")
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String forbidden() {
+        return "403";
+    }
 }
