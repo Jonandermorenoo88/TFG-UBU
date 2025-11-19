b98749000ddb1ef9e5061019d87180e03c8c330f
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-19 23:00:22 +0100

    refactor BloquesController.java: reorganización y limpieza del código

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/BloquesController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/BloquesController.java
@@ -1,5 +1,24 @@
+// CuestionarioController.java
 package es.ubu.gii.ISOAssetManager.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import es.ubu.gii.ISOAssetManager.repository.EmpresaRepository;
+
+@Controller
+@RequestMapping("/empresas")
 public class BloquesController {
-    
+
+    @Autowired
+    private EmpresaRepository empresaRepo;
+
+    @GetMapping("/{empresaId}/bloques")
+    public String elegirBloque(@PathVariable Long empresaId, Model model) {
+        model.addAttribute("empresa", empresaRepo.findById(empresaId).orElseThrow());
+        return "bloques";
+    }
 }
