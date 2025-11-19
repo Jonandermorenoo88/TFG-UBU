eb9cc2deb49e60677142b198c79acea8a08ab9bb
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-19 23:05:43 +0100

    creando GuiaController.java: implementación del controlador para la gestión de guías

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/GuiaController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/GuiaController.java
@@ -1,5 +1,16 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class GuiaController {
-    
+
+    // Ej.: /guia-control/A5.1  ->  templates/guias/control-A5-1.html
+    @GetMapping("/guia-control/{controlId:.+}")
+    public String verGuia(@PathVariable String controlId) {
+        String normalizado = controlId.replace('.', '-').toUpperCase(); // A5.1 -> A5-1
+        return "guias/control-" + normalizado;
+    }
 }
