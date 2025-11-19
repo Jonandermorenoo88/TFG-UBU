33b296b8605af61080993bc80c5221883bf229f5
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-20 00:12:49 +0100

    añadiendo la clase CsrfControllerAdvice para gestionar el token CSRF en la aplicación

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/config/CsrfControllerAdvice.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/config/CsrfControllerAdvice.java
@@ -1,6 +1,17 @@
 package es.ubu.gii.ISOAssetManager.config;
 
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class CsrfControllerAdvice {
-    // 
-    
+
+    @ModelAttribute("_csrf")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 }
