b90acc257d662a621160c85ec775fe7d05f6a6c5
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-20 00:16:47 +0100

    restaurando la interfaz ControlRepository para gestionar controles por categoría

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/ControlRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/ControlRepository.java
@@ -1,5 +1,10 @@
+// ControlRepository.java
 package es.ubu.gii.ISOAssetManager.repository;
 
-public class ControlRepository {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import es.ubu.gii.ISOAssetManager.model.Control;
+
+public interface ControlRepository extends JpaRepository<Control, String> {
+    List<Control> findByCategoria_IdOrderByOrdenAscIdAsc(String categoriaId);
 }
