b594e9c75598476f68b42576a389d667101eb085
jonandermorenoo88 <jaincera97@gmail.com>
2025-11-21 22:51:08 +0100

    añadiendo la interfaz EvidenciaRepository para la gestión de evidencias en el sistema

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/EvidenciaRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/EvidenciaRepository.java
@@ -1,5 +1,14 @@
 package es.ubu.gii.ISOAssetManager.repository;
 
-public class EvidenciaRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.ubu.gii.ISOAssetManager.model.Evidencia;
+
+public interface EvidenciaRepository extends JpaRepository<Evidencia, Long> {
+
+    // Para el map evidenciasPorPregunta[p.id] en CuestionarioController
+    List<Evidencia> findByEmpresa_IdAndControl_IdOrderByFechaSubidaDesc(Long empresaId, String controlId);
+
 }
