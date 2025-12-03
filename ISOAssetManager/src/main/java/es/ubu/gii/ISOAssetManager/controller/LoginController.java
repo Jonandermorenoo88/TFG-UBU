6b5e867f52479fe906c4587523e4db03df087c13
jonandermorenoo88 <jaincera97@gmail.com>
2025-12-04 00:43:01 +0100

    Agregar documentación en LoginController para clarificar métodos y mejorar la legibilidad del código.

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/LoginController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/LoginController.java
@@ -1,13 +1,22 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+/**
+ * Controlador para la gestión del inicio de sesión.
+ * <p>
+ * Maneja la visualización de la página de login personalizada.
+ * </p>
+ */
 @Controller
 public class LoginController {
 
+    /**
+     * Muestra el formulario de inicio de sesión.
+     *
+     * @return Nombre de la vista de login.
+     */
     @GetMapping("/login")
     public String mostrarLogin() {
         return "login";
