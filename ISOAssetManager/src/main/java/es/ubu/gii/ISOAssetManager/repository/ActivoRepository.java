c2ad8e75de7dba156b88f6a14302fbaa3e492fe1
jonandermorenoo88 <jaincera97@gmail.com>
2025-12-06 11:46:08 +0100

    Agregar documentación en la interfaz ActivoRepository para clarificar el propósito del repositorio y mejorar la legibilidad del código.

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/ActivoRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/ActivoRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.ubu.gii.ISOAssetManager.model.Activo;
 
+/**
+ * Repositorio para gestionar las operaciones CRUD de la entidad {@link Activo}.
+ */
 public interface ActivoRepository extends JpaRepository<Activo, Long> {
-    
-    // Buscar activos por ID de empresa
+
+    /**
+     * Busca todos los activos pertenecientes a una empresa específica.
+     *
+     * @param empresaId ID de la empresa.
+     * @return Lista de activos de la empresa.
+     */
     List<Activo> findByEmpresaId(Long empresaId);
 }
