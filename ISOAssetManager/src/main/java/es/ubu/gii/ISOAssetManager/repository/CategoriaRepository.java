3a71bbe6e41f3b6edf490314ad7289e1a7c21f19
jonandermorenoo88 <jaincera97@gmail.com>
2025-12-06 11:51:41 +0100

    Agregar documentación en la interfaz CategoriaRepository para clarificar métodos y mejorar la legibilidad del código.

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/CategoriaRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/CategoriaRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.ubu.gii.ISOAssetManager.model.Categoria;
 
+/**
+ * Repositorio para gestionar las operaciones CRUD de la entidad
+ * {@link Categoria}.
+ * <p>
+ * Permite acceder a las categorías de controles (ej. "A.5", "A.6") almacenadas
+ * en la base de datos.
+ * </p>
+ */
 public interface CategoriaRepository extends JpaRepository<Categoria, String> {
 }
-
