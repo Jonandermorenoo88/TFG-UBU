ac8397c9115e5304f78d9922343712cb0c69c4db
jonandermorenoo88 <jaincera97@gmail.com>
2025-12-06 13:17:53 +0100

    Agregar documentación en la interfaz EmpresaRepository para clarificar métodos y mejorar la legibilidad del código.

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/EmpresaRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/EmpresaRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.ubu.gii.ISOAssetManager.model.Empresa;
 
+/**
+ * Repositorio para gestionar las operaciones CRUD de la entidad
+ * {@link Empresa}.
+ * <p>
+ * Permite la gestión de las empresas registradas en el sistema.
+ * </p>
+ */
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 
 }
