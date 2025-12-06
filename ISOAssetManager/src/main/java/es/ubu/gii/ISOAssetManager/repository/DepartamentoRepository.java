31afe641c2780d6765713949f2cd9f5c010ced43
jonandermorenoo88 <jaincera97@gmail.com>
2025-12-06 11:55:39 +0100

    Agregar documentación en la interfaz DepartamentoRepository para clarificar métodos y mejorar la legibilidad del código.

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/DepartamentoRepository.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/repository/DepartamentoRepository.java
@@ -1,13 +1,25 @@
-// DepartamentoRepository.java
 package es.ubu.gii.ISOAssetManager.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.ubu.gii.ISOAssetManager.model.Departamento;
 
+/**
+ * Repositorio para gestionar las operaciones CRUD de la entidad
+ * {@link Departamento}.
+ * <p>
+ * Permite la gestión de departamentos dentro de las empresas.
+ * </p>
+ */
 public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
 
-    // Busca por descripción dentro de una empresa concreta
+    /**
+     * Busca un departamento por su descripción (nombre) dentro de una empresa
+     * específica.
+     *
+     * @param descripcion Nombre o descripción del departamento.
+     * @param empresaId   ID de la empresa a la que pertenece.
+     * @return El departamento si existe.
+     */
     Optional<Departamento> findByDescripcionAndEmpresaId(String descripcion, Long empresaId);
 }
-
